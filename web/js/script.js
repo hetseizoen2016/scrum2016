76e776c98956fdd11df3e33b6240d3cfb3b05ed6
samverhoeven <sam_verhoeven3@hotmail.com>
2016-04-05 15:41:54 +0200

    js inladen juiste volgorde + juiste window onload in script.js

--- web/js/script.js
+++ web/js/script.js
@@ -2,38 +2,34 @@
  * Created by Geert on 31/03/2016.
  */
 
-(function ($) {
-    $(function () {
+$(function () {
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('select').material_select();
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: false, // Displays dropdown below the button
+        alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+    );
 
-        $('.button-collapse').sideNav();
-        $('.parallax').parallax();
-        $('select').material_select();
-        $('.dropdown-button').dropdown({
-            inDuration: 300,
-            outDuration: 225,
-            constrain_width: false, // Does not change width of dropdown to that of the activator
-            hover: true, // Activate on hover
-            gutter: 0, // Spacing from edge
-            belowOrigin: false, // Displays dropdown below the button
-            alignment: 'left' // Displays dropdown with edge aligned to the left of button
-        }
-        );
+    //BXSLIDER INIT
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        pause: 8000,
+        speed: 1000,
+        auto: true,
+        controls: false
+    });
 
-        //BXSLIDER INIT
-        $('.bxslider').bxSlider({
-            mode: 'fade',
-            pause: 8000,
-            speed: 1000,
-            auto: true,
-            controls: false
-        });
+    $(".reservatie-form").steps({
+        headerTag: "h3",
+        bodyTag: "section",
+        transitionEffect: "slideLeft"
 
-        $(".reservatie-form").steps({
-            headerTag: "h3",
-            bodyTag: "section",
-            transitionEffect: "slideLeft"
-            
-        });
-
-    }); // end of document ready
-})(jQuery); // end of jQuery name space
+    });
+}); // end of document ready
